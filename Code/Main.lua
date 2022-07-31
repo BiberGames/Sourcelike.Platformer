726338da101090c290735f57be253364057449af
BENJA_303 <66800616+BiberGames@users.noreply.github.com>
2022-07-31 22:00:31 +0200

    Add files via upload

--- Code/Main.lua
+++ Code/Main.lua
@@ -13,12 +13,21 @@ function Start()
     Utils.DelayedLauncher("SpawnPlayer", 2)
 end
 
-function include()
-    CLConsole.Log(CLIO.LoadFileRaw("Addons/Platformer/Code/Autorun.lua"))
-    --local myModule = require(CLIO.GetStreamingAssetsPath() .. "/Addons/Platformer/Code/Autorun")
+function GeneratePlatform(Name, X, Y)
+    CLGameObject.CreateEmpty(Name)
+    CLGameObject.AddComponent(Name, "2D.Sprite")
+    CLGameObject.AddComponent(Name, "Physics.BoxCollider2D")
+    CL2D.SetBoxColliderSize(Name, 1.5, 1.5)
 
-    local myModule = dofile(CLIO.GetStreamingAssetsPath() .. "/Addons/Platformer/Code/Autorun")
-    myModule.Test("hallo!!!!!!!")
+    CL2D.SetSprite(Name, 12)
+    CLTransform.Position(Name, X, Y, 0, 0)
+end
+
+function GeneratePlatforms()
+    for py = 1, 30, 1
+    do
+        GeneratePlatform("Platform"..py, 10 * py / 2 - math.random(0, 1), 6 + math.random(-1, 2))
+    end
 end
 
 function CreateTestText()
@@ -66,6 +75,7 @@ function LoadSprites()
     CL2D.LoadSprite("Addons/Platformer/Textures/Player/Player_03.png", 9, 16, true)
     CL2D.LoadSprite("Addons/Platformer/Textures/Player/Player_04.png", 10, 16, true)
     CL2D.LoadSprite("Addons/Platformer/Textures/Football.png", 11, 16, true)
+    CL2D.LoadSprite("Addons/Platformer/Textures/World/Platform.png", 12, 32, true)
 end
 
 function CreateObject(Name, X, Y, Z)
@@ -75,13 +85,18 @@ function CreateObject(Name, X, Y, Z)
 end
 
 function SpawnPlayer()
-    CL2D.SetCapsuleColliderSize("2DPlayer", 1, 1)
+    CL2D.SetCapsuleColliderSize("2DPlayer", 0, 0)
+
+    CLGameObject.AddComponent("2DPlayer", "Physics.BoxCollider2D")
+    CL2D.SetBoxColliderSize("2DPlayer", 1, 1)
+
     CreateFootball()
     CreateIsland()
     CL2D.SetSprite("Sky", 6)
     CL2D.SetSprite("Tree", 5)
     CLTransform.Position("2DPlayer", 0, 10, 0, 0)
     CLCamera.SetSize("2DCamera", 5)
+    GeneratePlatforms()
     CLGameObject.SetActive("LoadingBackground", false)
 end
 
@@ -126,4 +141,11 @@ function Loop()
     if BallPosition[2] < 6 then
         CLTransform.Position("Football", -3, 15, 0, 0)
     end
+
+    PlayerPosition = CLTransform.GetPosition("2DPlayer")
+    if PlayerPosition[2] < 6 then
+        CLTransform.Position("2DPlayer", 0, 15, 0, 0)
+    end
+
+    CLTransform.Position("Sky", PlayerPosition[1], PlayerPosition[2], 10, 0)
 end
